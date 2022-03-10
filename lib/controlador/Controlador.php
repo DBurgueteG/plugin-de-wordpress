7c82ea93a3e09db09b4e5e0c6a95bb6fc5f5026b
DBurgueteG <dburguegar@educacion.navarra.es>
2022-03-10 14:03:49 +0100

    Admin: David: Creadas las reglas de validación

--- lib/controlador/Controlador.php
+++ lib/controlador/Controlador.php
@@ -3,7 +3,7 @@ add_shortcode('nuevo_Participantes', '');
 
 class Controlador
 {
-    function run()
+    public function run()
     {
         if (/*!isset($_POST) &&*/ $_POST[""] == "empezar") //no se ha enviado el formulario
         { // primera petición
@@ -19,8 +19,19 @@ class Controlador
         exit();
     }
 
-    function mostrarFormulario($fase, $validador, $resultado)
+    public function mostrarFormulario($fase, $validador, $resultado)
     {
         include "../../vistas/nuevoParticipante.php";
+    }
+
+    public function crearReglasValidacion()
+    {
+        $reglas = array(
+            "nombre" => array("required" => true, "maxLeng" => 150),
+            "correo" => array("required" => true, "maxLeng" => 100, "formato" => "email"),
+            "evento" => array("requiredEnviar" => true, "requiredRecuperar" => false),
+            "lenguajes" => array("requiredEnviar" => true, "requiredRecuperar" => false)
+        );
+        return $reglas;
     }
 }
